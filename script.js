e389a6f0187862b48c7998fc7e0ded694c9c48d2
trump090 <tmtrump0000@gmail.com>
2025-02-26 18:17:38 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("XcelCoin website loaded!");
+
+    const infoSection = document.getElementById("info");
+    if (infoSection) {
+        infoSection.innerHTML += "<p>Stay tuned for more updates!</p>";
+    }
+});

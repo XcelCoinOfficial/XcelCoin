719feefaa549c5670cc90a8d56bc5ad2685620dc
XcelCoinOfficial <tmtrump0000@gmail.com>
2025-03-01 12:38:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-const { Connection, PublicKey, Transaction, SystemProgram, sendAndConfirmTransaction } = solanaWeb3;
-
-const recipient = "CbUpHNo4T59NSX6FcmkDySLX6r5nmc7bWxPFJNqqaQiE"; // Phantom adresi
-const solAmount = amount * 0.01; // 1 XcelCoin = 0.01 SOL
-
-if (window.solana && window.solana.isConnected) {
-    try {
-        const transaction = new Transaction().add(
-            SystemProgram.transfer({
-                fromPubkey: window.solana.publicKey,
-                toPubkey: new PublicKey(recipient),
-                lamports: solAmount * 1000000000, // SOL -> Lamports
-            })
-        );
-
-        const { signature } = await window.solana.signAndSendTransaction(transaction);
-        alert(`Transaction sent! Signature: ${signature}`);
-        console.log("Transaction successful:", signature);
-    } catch (err) {
-        console.error("Transaction failed", err);
-        alert("Transaction failed: " + err.message);
-    }
-} else {
-    alert("Please connect your wallet first.");
-}
 // Presale Geri Sayım
 const countdownElement = document.getElementById("countdown");
-const presaleEndDate = new Date("March 10, 2025 00:00:00").getTime(); // Bitiş tarihi
+const presaleEndDate = new Date("March 10, 2025 00:00:00").getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
